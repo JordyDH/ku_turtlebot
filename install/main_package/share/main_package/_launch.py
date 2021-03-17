ebd19389666fbdd9d543bed5d44aabcdc3a25508
JordyDH <jordy.dehoon@hotmail.com>
2021-03-17 12:37:36 +0100

    Launch file fixed ready to push

--- install/main_package/share/main_package/_launch.py
+++ install/main_package/share/main_package/_launch.py
@@ -6,14 +6,10 @@ import launch_ros.actions
 
 def generate_launch_description():
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(
-            'node_prefix',
-            default_value=[launch.substitutions.EnvironmentVariable('USER'), '_'],
-            description='Prefix for node names'),
-        launch_ros.actions.Node(
-            package='main_package', executable='talker', output='screen',
-            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'talker']),
 	launch_ros.actions.Node(
-            package='main_package', executable='listener', output='screen',
-            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'listener']),
+		package='main_package', executable='talker', output='screen',
+		name=['talker']),
+	launch_ros.actions.Node(
+		package='main_package', executable='listener', output='screen',
+		name=['listener']),
     ])
